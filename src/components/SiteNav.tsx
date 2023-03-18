d9be16756f89ed8c8ecfb74b913540cd54e087ca
Joel Peckham <joelskyler@gmail.com>
2023-03-18 16:49:33 -0600

    error squashing. no SSR support in strict mode?

--- src/components/SiteNav.tsx
+++ src/components/SiteNav.tsx
@@ -3,7 +3,7 @@ import { Navbar, Button, Link, Text, Image, Spacer } from "@nextui-org/react";
 import React from "react";
 export const SiteNav = () => {
   return (
-    <Navbar variant="sticky">
+    <Navbar id="someNiceNav" variant="sticky">
       <Navbar.Brand>
         <Image src="/roadtripdesignerlogo.svg" width="25px" height="25px" />
         <Spacer x={0.5} />
@@ -11,12 +11,12 @@ export const SiteNav = () => {
           road trip designer
         </Text>
       </Navbar.Brand>
-      <Navbar.Content>
-          <Navbar.Link color="inherit" href="#">
+      <Navbar.Content id="averyniceContent">
+          <Navbar.Link color="inherit" href="#" id="averynicelink">
             Login
           </Navbar.Link>
-          <Navbar.Item>
-            <Button auto flat as={Link} href="#">
+          <Navbar.Item id="averycoolitem">
+            <Button auto flat as={Link} href="#" id="averynicebutton">
               Sign Up
             </Button>
           </Navbar.Item>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,32 +1,8 @@
 import Head from "next/head";
 import styles from "../styles/Home.module.css";
-// import React  from 'react';
-import {
-  Container,
-  Button,
-  Input,
-  Spacer,
-  Text,
-  Link,
-} from "@nextui-org/react";
 import { SiteNav } from "../components/SiteNav";
-import { app, database } from "../../firebase";
-import { collection, addDoc } from "firebase/firestore";
-import { useState } from "react";
 
 export default function Home() {
-  const [location, setLocation] = useState("");
-  const dbInstance = collection(database, "notes");
-  const saveLocation = async () => {
-    try {
-      const docRef = await addDoc(dbInstance, {
-        location: location,
-      });
-      console.log("Document written with ID: ", docRef.id);
-    } catch (e) {
-      console.error("Error adding document: ", e);
-    }
-  };
   return (
     <div className={styles.container}>
       <Head>
@@ -38,28 +14,6 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <SiteNav />
-      <Container>
-        <Text h1>Plan your next road trip with ease.</Text>
-        <Spacer y={1} />
-        <Text h3>Enter your starting location and destination.</Text>
-        <Spacer y={1} />
-        <Input
-          placeholder="Starting Location"
-          onChange={(e) => setLocation(e.target.value)}
-          label="Starting Location"
-        />
-        <Button auto flat onClick={() => saveLocation()}>
-          Submit
-        </Button>
-        <Spacer y={1} />
-        <Container className={styles.tall}></Container>
-        <Text>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque, maxime
-          fugit similique obcaecati magni placeat quae cupiditate beatae nihil
-          perspiciatis rem saepe, molestiae accusantium dolor odit blanditiis
-          officiis molestias nostrum?
-        </Text>
-      </Container>
     </div>
   );
 }
