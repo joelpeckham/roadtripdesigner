3e606617b1293b234107f9e557f645d2c1e6028e
Joel Peckham <joelskyler@gmail.com>
2023-03-19 09:12:05 -0600

    removed mantine

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,5 @@
 import Head from "next/head";
 import styles from "../styles/Home.module.css";
-import { HeaderAction } from "@/components/HeaderAction";
 export default function Home() {
   return (
     <div className={styles.container}>
@@ -12,7 +11,6 @@ export default function Home() {
         />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <HeaderAction links={[{link:"#", label:"this", links:[]}]} />
     </div>
   );
 }
