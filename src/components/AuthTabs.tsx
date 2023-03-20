d0baad4ff9ce60e54f1bfa7b87f42f3be97bcd84
Joel Peckham <joelskyler@gmail.com>
2023-03-20 14:33:05 -0600

    loginpage try three -- hooks man!

--- src/components/AuthTabs.tsx
+++ src/components/AuthTabs.tsx
@@ -3,14 +3,17 @@ import { TabView, TabPanel } from "primereact/tabview";
 import { RegisterForm } from "@/components/RegisterForm";
 import { LoginForm } from "@/components/LoginForm";
 import { useRouter } from "next/router";
-const router = useRouter();
+
 interface AuthTabsProps {
-  closeModal: (e: React.MouseEvent<HTMLButtonElement, MouseEvent> | null | undefined) => void;
+  closeModal: (
+    e: React.MouseEvent<HTMLButtonElement, MouseEvent> | null | undefined
+  ) => void;
   redirect?: string;
 }
-export const AuthTabs = ({closeModal, redirect}:AuthTabsProps) => {
+export const AuthTabs = ({ closeModal, redirect }: AuthTabsProps) => {
+  const router = useRouter();
   if (redirect) {
-    closeModal  = () => router.push(redirect);
+    closeModal = () => router.push(redirect);
   }
   const [activeTabIndex, setActiveTabIndex] = useState(0);
   return (
@@ -20,12 +23,12 @@ export const AuthTabs = ({closeModal, redirect}:AuthTabsProps) => {
     >
       <TabPanel header="Login">
         <div className="pl-2 pr-2">
-          <LoginForm closeModal={closeModal}/>
+          <LoginForm closeModal={closeModal} />
         </div>
       </TabPanel>
       <TabPanel header="Register">
         <div className="pl-2 pr-2">
-          <RegisterForm closeModal={closeModal}/>
+          <RegisterForm closeModal={closeModal} />
         </div>
       </TabPanel>
     </TabView>
